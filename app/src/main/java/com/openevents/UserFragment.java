97fa1e81a1ffa495e0370b73b9da083eb2c5a242
alex.luelmo <alex.luelmo@students.salle.url.edu>
2022-05-19 19:03:12 +0200

    minor layout changes

--- app/src/main/java/com/openevents/UserFragment.java
+++ app/src/main/java/com/openevents/UserFragment.java
@@ -2,11 +2,18 @@ package com.openevents;
 
 import android.os.Bundle;
 
+import androidx.annotation.MainThread;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -53,6 +60,7 @@ public class UserFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        setHasOptionsMenu(true);
     }
 
     @Override
