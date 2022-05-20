5b239a577f0bf1783a9b5e2d91343e63cd3b5424
alex.luelmo <alex.luelmo@students.salle.url.edu>
2022-05-20 03:52:13 +0200

    creation of my events fragment and minor improvements of events item

--- app/src/main/java/com/openevents/EventsFragment.java
+++ app/src/main/java/com/openevents/EventsFragment.java
@@ -14,52 +14,17 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EventsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class EventsFragment extends Fragment implements EventsRecyclerViewAdapter.ItemClickListener{
-    private EventsRecyclerViewAdapter adapter;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class EventsFragment extends Fragment implements MyEventsRecyclerViewAdapter.ItemClickListener{
+    private MyEventsRecyclerViewAdapter adapter;
 
     public EventsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EventsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EventsFragment newInstance(String param1, String param2) {
-        EventsFragment fragment = new EventsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -93,10 +58,18 @@ public class EventsFragment extends Fragment implements EventsRecyclerViewAdapte
         eventsLocation.add("Perú");
         eventsLocation.add("Perú");
 
+        ArrayList<String> eventsDate = new ArrayList<>();
+        eventsDate.add("12/12/2020");
+        eventsDate.add("12/12/2020");
+        eventsDate.add("12/12/2020");
+        eventsDate.add("12/12/2020");
+        eventsDate.add("12/12/2020");
+        eventsDate.add("12/12/2020");
+
         // set up the vertical RecyclerView
         RecyclerView recyclerView = view.findViewById(R.id.AllEventsRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-        adapter = new EventsRecyclerViewAdapter(getContext(), eventsImage, eventsTitle, eventsLocation);
+        adapter = new MyEventsRecyclerViewAdapter(getContext(), eventsImage, eventsTitle, eventsLocation, eventsDate);
         adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
 
--- app/src/main/java/com/openevents/MyEventsRecyclerViewAdapter.java
+++ app/src/main/java/com/openevents/MyEventsRecyclerViewAdapter.java
@@ -11,39 +11,42 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class EventsRecyclerViewAdapter extends RecyclerView.Adapter<EventsRecyclerViewAdapter.ViewHolder> {
+public class MyEventsRecyclerViewAdapter extends RecyclerView.Adapter<MyEventsRecyclerViewAdapter.ViewHolder> {
 
     private List<Integer> mImage;
     private List<String> mTitle;
     private List<String> mLocation;
+    private List<String> mDate;
     private LayoutInflater mInflater;
-    private ItemClickListener mClickListener;
+    private MyEventsRecyclerViewAdapter.ItemClickListener mClickListener;
 
-
-    public EventsRecyclerViewAdapter(Context context, List<Integer> images, List<String> titles, List<String> locations) {
+    public MyEventsRecyclerViewAdapter(Context context, List<Integer> images, List<String> titles, List<String> locations, List<String> dates) {
         this.mInflater = LayoutInflater.from(context);
         this.mImage = images;
         this.mTitle = titles;
         this.mLocation = locations;
+        this.mDate = dates;
     }
 
     // inflates the row layout from xml when needed
     @Override
     @NonNull
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MyEventsRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = mInflater.inflate(R.layout.events_recyclerview_item, parent, false);
-        return new ViewHolder(view);
+        return new MyEventsRecyclerViewAdapter.ViewHolder(view);
     }
 
     // binds the data to the view and textview in each row
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyEventsRecyclerViewAdapter.ViewHolder holder, int position) {
         int i = mImage.get(position);
         String title = mTitle.get(position);
         String location = mLocation.get(position);
+        String date = mDate.get(position);
         holder.myEventsView.setBackgroundColor(i);
         holder.myTextView.setText(title);
         holder.myLocationView.setText(location);
+        holder.myDateView.setText(date);
     }
 
     // total number of rows
@@ -57,12 +60,14 @@ public class EventsRecyclerViewAdapter extends RecyclerView.Adapter<EventsRecycl
         View myEventsView;
         TextView myTextView;
         TextView myLocationView;
+        TextView myDateView;
 
         ViewHolder(View itemView) {
             super(itemView);
             myEventsView = itemView.findViewById(R.id.EventsRecyclerViewEventImage);
             myTextView = itemView.findViewById(R.id.EventsRecyclerViewEventTitle);
             myLocationView = itemView.findViewById(R.id.EventsRecyclerViewEventLocation);
+            myDateView = itemView.findViewById(R.id.EventsRecyclerViewEventDate);
             itemView.setOnClickListener(this);
         }
 
@@ -78,7 +83,7 @@ public class EventsRecyclerViewAdapter extends RecyclerView.Adapter<EventsRecycl
     }
 
     // allows clicks events to be caught
-    public void setClickListener(ItemClickListener itemClickListener) {
+    public void setClickListener(MyEventsRecyclerViewAdapter.ItemClickListener itemClickListener) {
         this.mClickListener = itemClickListener;
     }
 
