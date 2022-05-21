c55942f839adb652db67a78893ae7fbcd5147cef
alex.luelmo <alex.luelmo@students.salle.url.edu>
2022-05-22 00:36:23 +0200

    added a event category carrousel to home event

---
+++ app/src/main/java/com/openevents/model/adapters/CategoriesAdapter.java
@@ -0,0 +1,48 @@
+package com.openevents.model.adapters;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.openevents.R;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.ViewHolder> {
+    private String[] mDataSet;
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textView;
+        public ViewHolder(ViewGroup parent) {
+            super(LayoutInflater.from(parent.getContext()).inflate(R.layout.event_type_item, parent, false));
+            textView = itemView.findViewById(R.id.type_title);
+        }
+    }
+
+    public CategoriesAdapter(String[] myDataSet) {
+        mDataSet = myDataSet;
+    }
+
+    @NonNull
+    @Override
+    public CategoriesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                           int viewType) {
+
+        return new CategoriesAdapter.ViewHolder(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView.setText(mDataSet[position]);
+    }
+
+    // Método que define la cantidad de elementos del RecyclerView
+    // Puede ser más complejo (por ejem, si implementamos filtros o búsquedas)
+    @Override
+    public int getItemCount() {
+        return mDataSet.length;
+    }
+}
